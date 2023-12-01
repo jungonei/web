f2bbfb8dce8986278901ff48fb6bde7e54de5658
jungonei <129926071+jungonei@users.noreply.github.com>
2023-12-01 02:30:52 +0000

    update

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 from sqlalchemy import text
 
-list_doctor = ['', 'dr. Nurita', 'dr. Yogi', 'dr. Wibowo', 'dr. Ulama', 'dr. Ping']
-list_symptom = ['', 'male', 'female']
+list_jenis_kelamin = ['Perempuan', 'Laki-Laki']
 
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://helvytianarn:5uEaoKh2VFGP@ep-shy-frog-96637425.us-east-2.aws.neon.tech/web")
 with conn.session as session:
-    query = text('CREATE TABLE IF NOT EXISTS SCHEDULE (id serial, doctor_name varchar, patient_name varchar, gender char(25), \
-                                                       symptom text, handphone varchar, address text, tanggal date);')
+    query = text('CREATE TABLE IF NOT EXISTS MAHASISWA (id serial, nama varchar, nrp varchar, jenis_kelamin varchar, \
+                                                       tempat_lahir text, tanggal_lahir date, asal text, alamat_domisili text);')
     session.execute(query)
 
 st.header('SIMPLE HOSPITAL DATA MANAGEMENT SYSTEM')
@@ -21,51 +20,49 @@ if page == "View Data":
 if page == "Edit Data":
     if st.button('Tambah Data'):
         with conn.session as session:
-            query = text('INSERT INTO schedule (doctor_name, patient_name, gender, symptom, handphone, address, waktu, tanggal) \
-                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8);')
-            session.execute(query, {'1':'', '2':'', '3':'', '4':'[]', '5':'', '6':'', '7':None, '8':None})
+            query = text('INSERT INTO MAHASISWA (nama, nrp, jenis_kelamin, tempat_lahir, tanggal_lahir, asal, alamat_domisili) \
+                          VALUES (:1, :2, :3, :4, :5, :6, :7);')
+            session.execute(query, {'1':'', '2':'', '3':'', '4':'', '5':None, '6':'', '7':''})
             session.commit()
 
-    data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0")
+    data = conn.query('SELECT * FROM MAHASISWA ORDER By id;', ttl="0")
     for _, result in data.iterrows():        
         id = result['id']
-        doctor_name_lama = result["doctor_name"]
-        patient_name_lama = result["patient_name"]
-        gender_lama = result["gender"]
-        symptom_lama = result["symptom"]
-        handphone_lama = result["handphone"]
-        address_lama = result["address"]
-        waktu_lama = result["waktu"]
-        tanggal_lama = result["tanggal"]
+        nama_lama = result["nama"]
+        nrp_lama = result["nrp"]
+        jenis_kelamin_lama = result["jenis_kelamin"]
+        tempat_lahir_lama = result["tempat_lahir"]
+        tanggal_lahir_lama = result["tanggal_lahir"]
+        asal_lama = result["asal"]
+        alamat_domisili_lama = result["alamat_domisili"]
 
-        with st.expander(f'a.n. {patient_name_lama}'):
+        with st.expander(f'a.n. {nama_lama}'):
             with st.form(f'data-{id}'):
-                doctor_name_baru = st.selectbox("doctor_name", list_doctor, list_doctor.index(doctor_name_lama))
-                patient_name_baru = st.text_input("patient_name", patient_name_lama)
-                gender_baru = st.selectbox("gender", list_symptom, list_symptom.index(gender_lama))
-                symptom_baru = st.multiselect("symptom", ['cough', 'flu', 'headache', 'stomache'], eval(symptom_lama))
-                handphone_baru = st.text_input("handphone", handphone_lama)
-                address_baru = st.text_input("address", address_lama)
-                waktu_baru = st.time_input("waktu", waktu_lama)
-                tanggal_baru = st.date_input("tanggal", tanggal_lama)
+                nama_baru = st.text_input("nama", nama_lama)
+                nrp_baru = st.text_input("nrp", nrp_lama)
+                jenis_kelamin_baru = st.selectbox("jenis_kelamin", list_jenis_kelamin, list_jenis_kelamin.index(jenis_kelamin_lama))
+                tempat_lahir_baru = st.text_input("tempat_lahir", eval(tempat_lahir_lama))
+                tanggal_lahir_baru = st.text_input("tanggal_lahir", tanggal_lahir_lama)
+                asal_baru = st.text_input("asal", asal_lama)
+                alamat_domisili_baru = st.text_input("alamat_domisili", alamat_domisili_lama)
                 
                 col1, col2 = st.columns([1, 6])
 
                 with col1:
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
-                            query = text('UPDATE schedule \
-                                          SET doctor_name=:1, patient_name=:2, gender=:3, symptom=:4, \
-                                          handphone=:5, address=:6, waktu=:7, tanggal=:8 \
-                                          WHERE id=:9;')
-                            session.execute(query, {'1':doctor_name_baru, '2':patient_name_baru, '3':gender_baru, '4':str(symptom_baru), 
-                                                    '5':handphone_baru, '6':address_baru, '7':waktu_baru, '8':tanggal_baru, '9':id})
+                            query = text('UPDATE MAHASISWA \
+                                          SET nama=:1, nrp=:2, jenis_kelamin=:3, tempat_lahir=:4, \
+                                          tanggal_lahir=:5, asal=:6, alamat_domisili=:7 \
+                                          WHERE id=:8;')
+                            session.execute(query, {'1':nama_baru, '2':nrp_baru, '3':jenis_kelamin_baru, '4':tempat_lahir_baru, 
+                                                    '5':tanggal_lahir_baru, '6':asal_baru, '7':alamat_domisili_baru, '8':id})
                             session.commit()
                             st.experimental_rerun()
                 
                 with col2:
                     if st.form_submit_button('DELETE'):
-                        query = text(f'DELETE FROM schedule WHERE id=:1;')
+                        query = text(f'DELETE FROM MAHASISWA WHERE id=:1;')
                         session.execute(query, {'1':id})
                         session.commit()
                         st.experimental_rerun()
